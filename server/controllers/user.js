64133a6169c453d5404180d01b2446e80d812627
June desktop <kain1252@naver.com>
2023-02-28 18:30:18 +0900

    Fix: jwt secret key 수정 및 .env로 이동

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,9 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/user.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const signin = async (req, res) => {
     const {email, password} = req.body;
@@ -15,7 +18,7 @@ export const signin = async (req, res) => {
 
         if(!isPasswordCorrect) return res.status(400).json({message: "Invalid credentails"});
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.ACCESS_SECRET, { expiresIn: "1h" });
 
         res.status(200).json({ result: existingUser, token });
 
@@ -38,7 +41,7 @@ export const signup = async (req, res) => {
 
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
 
-        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" });
+        const token = jwt.sign({ email: result.email, id: result._id }, process.env.ACCESS_SECRET, { expiresIn: "1h" });
 
         res.status(201).json({ result, token });
     } catch (error) {
